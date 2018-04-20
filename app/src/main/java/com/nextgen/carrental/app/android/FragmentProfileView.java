31c1277d0eec8e555eddd6ba1ab273d0460e96d7
Prithwish Ghosh <Prithwish.Ghosh@ehi.com>
2018-04-20 18:32:20 +0530

    Adding Profile view

--- app/src/main/java/com/nextgen/carrental/app/android/FragmentProfileView.java
+++ app/src/main/java/com/nextgen/carrental/app/android/FragmentProfileView.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.nextgen.carrental.app.R;
 
@@ -21,6 +23,11 @@ public class FragmentProfileView extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile_view, container, false);
+        Spinner spinnerCarTypes = view.findViewById(R.id.spinnerCarType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource (
+                getActivity(), R.array.car_type_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCarTypes.setAdapter(adapter);
         return view;
     }
 
