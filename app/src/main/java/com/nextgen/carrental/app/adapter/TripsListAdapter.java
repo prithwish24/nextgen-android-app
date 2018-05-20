2378119b241b5271c03c61804909b445019ea639
abh1989 <abhdutta3981@gmail.com>
2018-05-21 01:04:57 +0530

    Trips flow coding done . Testing not yet complete.

---
+++ app/src/main/java/com/nextgen/carrental/app/adapter/TripsListAdapter.java
@@ -0,0 +1,90 @@
+package com.nextgen.carrental.app.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nextgen.carrental.app.R;
+import com.nextgen.carrental.app.model.BookingData;
+import com.nextgen.carrental.app.model.CarClassEnum;
+import com.nextgen.carrental.app.util.Utils;
+
+import java.util.List;
+
+
+public class TripsListAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+    private List<BookingData> mDataSource;
+
+    public TripsListAdapter(Context mContext, List<BookingData> tripsList) {
+        //this.mContext = mContext;
+        this.mDataSource = tripsList;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mDataSource.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDataSource.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public void setItemList(List<BookingData> itemList) {
+        this.mDataSource = itemList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TripsListAdapter.ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.fragment_home, parent, false);
+
+            holder = new TripsListAdapter.ViewHolder();
+            holder.thumbnailImageView = convertView.findViewById(R.id.car_image_home);
+            holder.carTypeTextView  = convertView.findViewById(R.id.car_type_home);
+            holder.pickUpTextView = convertView.findViewById(R.id.pickup_point_home);
+            holder.pickUpTimeTextView = convertView.findViewById(R.id.pickup_time_home);
+            holder.returnTimeTextView = convertView.findViewById(R.id.drop_time_home);
+
+            convertView.setTag(holder);
+
+        } else {
+
+            holder = (TripsListAdapter.ViewHolder) convertView.getTag();
+        }
+
+        final BookingData trip = (BookingData) getItem(position);
+        holder.carTypeTextView.setText(trip.carType);
+        holder.pickUpTextView.setText(trip.pickupLoc);
+        holder.pickUpTimeTextView.setText(Utils.fmtTime(trip.pickupDateTime, Utils.LONG_DATE_TIME));
+        holder.returnTimeTextView.setText(Utils.fmtTime(trip.pickupDateTime, Utils.LONG_DATE_TIME));
+        final CarClassEnum imgId = CarClassEnum.find(trip.carType);
+        if (imgId != null) {
+            holder.thumbnailImageView.setImageResource(imgId.getImgId());
+        }
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        ImageView thumbnailImageView;
+        TextView carTypeTextView;
+        TextView pickUpTextView;
+        TextView pickUpTimeTextView;
+        TextView returnTimeTextView;
+    }
+}
--- app/src/main/java/com/nextgen/carrental/app/android/FragmentHome.java
+++ app/src/main/java/com/nextgen/carrental/app/android/FragmentHome.java
@@ -1,18 +1,39 @@
 package com.nextgen.carrental.app.android;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.nextgen.carrental.app.R;
+import com.nextgen.carrental.app.adapter.ReservationListAdapter;
+import com.nextgen.carrental.app.adapter.TripsListAdapter;
+import com.nextgen.carrental.app.bo.BaseResponse;
+import com.nextgen.carrental.app.bo.TripsResponse;
+import com.nextgen.carrental.app.constants.GlobalConstants;
+import com.nextgen.carrental.app.model.BookingData;
+import com.nextgen.carrental.app.service.RestClient;
+import com.nextgen.carrental.app.service.RestException;
+import com.nextgen.carrental.app.service.RestParameter;
+import com.nextgen.carrental.app.util.Utils;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ai.api.model.AIRequest;
 
 
 /**
@@ -32,31 +53,74 @@ public class FragmentHome extends Fragment {
         if (appCompatActivity.getSupportActionBar() != null) {
             appCompatActivity.getSupportActionBar().show();
         }
+
         return homeView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        final ImageView carClassImage = homeView.findViewById(R.id.car_image_home);
-        carClassImage.setImageResource(R.drawable.ct_compact);
-        carClassImage.refreshDrawableState();
-
-        FloatingActionButton fab = (FloatingActionButton) homeView.findViewById(R.id.fab);
+        final ListView listView = homeView.findViewById(R.id.show_trips_list_view);
+        TripsListAdapter adapter = new TripsListAdapter(getActivity(), new ArrayList<BookingData>());
+        listView.setAdapter(adapter);
+        FloatingActionButton fab = homeView.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 startActivity(new Intent(getActivity().getApplicationContext(), VoiceChatActivity.class));
             }
         });
+        new GetMyTrips(getActivity().getApplicationContext(),view,adapter).execute();
+    }
 
+    private class GetMyTrips extends AsyncTask<Void, Void, BaseResponse<BookingData[]>> {
 
+        private WeakReference<Context> contextRef;
+        private WeakReference<View> viewRef;
+        private TripsListAdapter adapter;
 
+        GetMyTrips(Context context, View view, TripsListAdapter adapter) {
+            this.contextRef = new WeakReference<>(context);
+            this.viewRef = new WeakReference<>(view);
+            this.adapter = adapter;
+        }
+        @Override
+        protected BaseResponse<BookingData[]> doInBackground(Void... requests) {
+            BaseResponse<BookingData[]> response = null;
+            SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences(GlobalConstants.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+            String sessionId = pref.getString(GlobalConstants.KEY_SESSIONID,"");
+            String userId = pref.getString(GlobalConstants.KEY_USERID,"");
+            try {
+                RestParameter data = new RestParameter();
+                data.addQueryParam("sessionId", sessionId);
+                data.addQueryParam("username", userId);
+                response = RestClient.INSTANCE.getRequest(
+                        Utils.getServiceURL(contextRef.get(),
+                                GlobalConstants.Services.UPCOMING_RESERVATION), BookingData[].class, data);
+            } catch (RestException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+            return response;
+        }
 
+        @Override
+        protected void onPostExecute(BaseResponse<BookingData[]> response) {
+            BookingData[] trips = response.getResponse();
+            List<BookingData> bookingDataList = Arrays.asList(trips);
+            if (bookingDataList == null || bookingDataList.size() == 0) {
 
+                final View viewMessage = viewRef.get().findViewById(R.id.no_trip_list_view);
+                viewMessage.setVisibility(View.VISIBLE);
+
+                final View viewList = viewRef.get().findViewById(R.id.show_trips_list_view);
+                viewList.setVisibility(View.GONE);
+
+            } else {
+                adapter.setItemList(bookingDataList);
+                adapter.notifyDataSetChanged();
+            }
+
+        }
     }
 
 }
